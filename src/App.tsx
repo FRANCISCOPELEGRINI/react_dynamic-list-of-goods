352440f798080457db1e212a1708c2e7af8756f4
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2026-03-23 19:15:14 -0300

    React-dynamic-list 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
-import { useState } from 'react';
-
-// import { getAll, get5First, getRed } from './api/goods';
-// or
 import * as goodsAPI from './api/goods';
 
 export const App: React.FC = () => {
-  const [goodsList, setgoodsList] = useState<Good[]>([]);
+  const [goodsList, setGoodsList] = useState<Good[]>([]);
 
-  const loadAllgoodsList = () => {
+  const loadAllGoods = () => {
     goodsAPI
       .getAll()
-      .catch()
-      .then(data => setgoodsList(data));
+      .then(data => {
+        setGoodsList(data);
+      })
+      .catch(() => {
+        alert('Erro ao carregar produtos.');
+      });
   };
 
-  const loadFiveFirtsgoodsList = () => {
+  const loadFiveFirstGoods = () => {
     goodsAPI
       .get5First()
-      .catch()
-      .then(data => setgoodsList(data));
+      .then(data => {
+        setGoodsList(data);
+      })
+      .catch(() => {
+        alert('Erro ao carregar produtos.');
+      });
   };
 
-  const loadRedgoodsList = () => {
+  const loadRedGoods = () => {
     goodsAPI
       .getRedGoods()
-      .catch()
-      .then(data => setgoodsList(data));
+      .then(data => {
+        setGoodsList(data);
+      })
+      .catch(() => {
+        alert('Erro ao carregar produtos.');
+      });
   };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => loadAllgoodsList()}
-      >
+      <button type="button" data-cy="all-button" onClick={loadAllGoods}>
         Load all goods
       </button>
 
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => loadFiveFirtsgoodsList()}
+        onClick={loadFiveFirstGoods}
       >
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => loadRedgoodsList()}
-      >
+      <button type="button" data-cy="red-button" onClick={loadRedGoods}>
         Load red goods
       </button>
-      <h1></h1>
+
       <GoodsList goods={goodsList} />
     </div>
   );
